d453b78f1d681605a8493e49d59949bec4723ba9
nitroflap <fe.lap.prog@gmail.com>
2021-10-03 02:25:25 +0300

    feat(context): message commands context

--- context.go
+++ context.go
@@ -1,6 +1,10 @@
 package disgolf
 
-import "github.com/bwmarrin/discordgo"
+import (
+	"strings"
+
+	"github.com/bwmarrin/discordgo"
+)
 
 // OptionsMap is an alias for map of discordgo.ApplicationCommandInteractionDataOption
 type OptionsMap = map[string]*discordgo.ApplicationCommandInteractionDataOption
@@ -41,4 +45,43 @@ func makeOptionMap(options []*discordgo.ApplicationCommandInteractionDataOption)
 	}
 
 	return
+}
+
+// MessageCtx is a context provided to message command handler. It contains the message
+type MessageCtx struct {
+	*discordgo.Session
+	Message   *discordgo.Message
+	Arguments []string
+}
+
+// Reply sends and returns a simple (content-only) message replying to the command message. If mention is true the command author is mentioned in the reply.
+// It is a wrapper for discordgo.Session.ChannelMessageSendReply.
+func (ctx *MessageCtx) Reply(content string, mention bool) (*discordgo.Message, error) {
+	return ctx.ReplyComplex(&discordgo.MessageSend{
+		Content: content,
+	}, mention)
+}
+
+// ReplyComplex sends and returns a complex (with embds, attachments, etc) message replying to the command message.
+// If mention is true the command author is mentioned in the reply. It is a wrapper for discordgo.Session.ChannelMessageSendComplex
+func (ctx *MessageCtx) ReplyComplex(message *discordgo.MessageSend, mention bool) (*discordgo.Message, error) {
+	message.Reference = ctx.Message.Reference()
+	// TODO: https://github.com/bwmarrin/discordgo/pull/1009
+	// message.AllowedMentions.RepliedUser = mention
+
+	return ctx.Session.ChannelMessageSendComplex(ctx.Message.ChannelID, message)
+}
+
+// NewMessageCtx constructs context from a message.
+// If argdelim is not empty it is a delimiter for the arguments, otherwise the arguments are split by a space.
+func NewMessageCtx(s *discordgo.Session, m *discordgo.Message, argdelim string) *MessageCtx {
+	if argdelim == "" {
+		argdelim = " "
+	}
+
+	return &MessageCtx{
+		Session:   s,
+		Message:   m,
+		Arguments: strings.Split(m.Content, argdelim),
+	}
 }
